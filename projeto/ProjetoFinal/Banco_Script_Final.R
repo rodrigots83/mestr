da5feed1ea69c0c75a469f3e8b91fbac2195e720
rodrigots83 <santos.rodrigo.teixeira@gmail.com>
2019-11-06 07:54:02 -0300

    Atualização do trabalho final

--- projeto/ProjetoFinal/Banco_Script_Final.R
+++ projeto/ProjetoFinal/Banco_Script_Final.R
@@ -24,137 +24,156 @@ library(mlbench)
 library(caretEnsemble)
 library(rattle)
 
+###########################################################################
+###########################################################################
 ### FUNÇÃO DE CRIAÇÃO DE ARQUIVO
-gera_arquivo_grafico = function(tituloGrafico){
-  nomeArquivo <- str_c(tituloGrafico,".jpeg")
-  ggsave(filename = nomeArquivo, plot = last_plot(), path = getwd(), width = )
-}
+# gera_arquivo_grafico = function(tituloGrafico){
+#   nomeArquivo <- str_c(tituloGrafico,".jpeg")
+#   ggsave(filename = nomeArquivo, plot = last_plot(), path = getwd(), width = )
+# }
 
+###########################################################################
+###########################################################################
 ### FUNÇÃO
-box_plot = function(data, atributos, atributosSubgrupo, variavelDependente = 'Evadiu'){
-  ### SETADO LOCAL PARA GUARDAR OS GRAFICOS GERADOS
-  setwd("/Users/rodrigosantos/Documents/GitHub/weak_signals/projeto/ProjetoFinal/graficos_gerados/")
-  for (atributo in atributos) {
-    if(length(atributosSubgrupo) > 0){
-      for (atributo2 in atributosSubgrupo){
-        result = ggplot(data = dtAmostra, aes_string(variavelDependente, atributo)) +
-          geom_boxplot(aes_string(fill = atributo2))
-        # geom_boxplot() +
-        labs(x = variavelDependente,
-             y = atributo,
-             title = "Boxplot",
-             subtitle = paste(atributo, variavelDependente, sep = " vs "),
-             caption = "") +
-          theme_bw()
-        # facet_wrap(~ atributo2)
-        print(result)
-        ### FUNCAO PARA GRAVAR OS RELATORIOS EM ARQUIVOS
-        gera_arquivo_grafico(str_c(atributo, variavelDependente, atributo2))
-      }
-    }
-    else{
-      result = ggplot(data = dtAmostra, aes_string(variavelDependente, atributo)) +
-        geom_boxplot() +
-        labs(x = variavelDependente,
-             y = atributo,
-             title = "Boxplot",
-             subtitle = paste(atributo, variavelDependente, sep = " vs "),
-             caption = "") +
-        theme_bw()
-      # facet_wrap(~ atributo2)
-      print(result)
-      ### FUNCAO PARA GRAVAR OS RELATORIOS EM ARQUIVOS
-      gera_arquivo_grafico(str_c(atributo, variavelDependente, atributo2))
-    }
-  }
-}
+# box_plot = function(data, atributos, atributosSubgrupo, variavelDependente = 'Evadiu'){
+#   ### SETADO LOCAL PARA GUARDAR OS GRAFICOS GERADOS
+#   setwd("/Users/rodrigosantos/Documents/GitHub/weak_signals/projeto/ProjetoFinal/graficos_gerados/")
+#   for (atributo in atributos) {
+#     if(length(atributosSubgrupo) > 0){
+#       for (atributo2 in atributosSubgrupo){
+#         result = ggplot(data = dtAmostra, aes_string(variavelDependente, atributo)) +
+#           geom_boxplot(aes_string(fill = atributo2))
+#         # geom_boxplot() +
+#         labs(x = variavelDependente,
+#              y = atributo,
+#              title = "Boxplot",
+#              subtitle = paste(atributo, variavelDependente, sep = " vs "),
+#              caption = "") +
+#           theme_bw()
+#         # facet_wrap(~ atributo2)
+#         print(result)
+#         ### FUNCAO PARA GRAVAR OS RELATORIOS EM ARQUIVOS
+#         gera_arquivo_grafico(str_c(atributo, variavelDependente, atributo2))
+#       }
+#     }
+#     else{
+#       result = ggplot(data = dtAmostra, aes_string(variavelDependente, atributo)) +
+#         geom_boxplot() +
+#         labs(x = variavelDependente,
+#              y = atributo,
+#              title = "Boxplot",
+#              subtitle = paste(atributo, variavelDependente, sep = " vs "),
+#              caption = "") +
+#         theme_bw()
+#       # facet_wrap(~ atributo2)
+#       print(result)
+#       ### FUNCAO PARA GRAVAR OS RELATORIOS EM ARQUIVOS
+#       gera_arquivo_grafico(str_c(atributo, variavelDependente, atributo2))
+#     }
+#   }
+# }
 
+###########################################################################
+###########################################################################
 ### Leitura do arquivo feita e análise dos dados
 
 dtAmostra <- fread("dadosBanco/Amostra_Modelo_Evasao_Correntistas_v3.csv")
-
 str(dtAmostra)
 
+###########################################################################
+###########################################################################
 ### Transformação das variáveis em fatores
 
-dtAmostra = 
-  dtAmostra %>% 
-  mutate(Evadiu = ifelse(Evadiu == 1, "Sim", "Não")) %>% 
-  mutate(Segmento = ifelse(Segmento == "GV", "Renda_Basica", ifelse(Segmento == "GC", "Classe_Media", ifelse(Segmento == "GR", "Alta_Renda", "Exclusivo"))))
+# dtAmostra = 
+#   dtAmostra %>% 
+#   mutate(Evadiu = ifelse(Evadiu == 1, "Sim", "Não")) %>% 
+#   mutate(Segmento = ifelse(Segmento == "GV", "Renda_Basica", ifelse(Segmento == "GC", "Classe_Media", ifelse(Segmento == "GR", "Alta_Renda", "Exclusivo"))))
+# 
+# dtAmostra$Evadiu <- as.factor(dtAmostra$Evadiu)
+# dtAmostra$Segmento <- as.factor(dtAmostra$Segmento)
+# dtAmostra$Debito_Automatico <- as.factor(dtAmostra$Debito_Automatico)
+# dtAmostra$Credito_Salario <- as.factor(dtAmostra$Credito_Salario)
+# dtAmostra$Credenciamento <- as.factor(dtAmostra$Credenciamento)
+# dtAmostra$Caixa_Seguradora <- as.factor(dtAmostra$Caixa_Seguradora)
+# dtAmostra$Pediu_Portabilidade <- as.factor(dtAmostra$Pediu_Portabilidade)
+# dtAmostra$Abriu_Reclamacao <- as.factor(dtAmostra$Abriu_Reclamacao)
+# dtAmostra$Debito_Automatico_DIF <- as.factor(dtAmostra$Debito_Automatico_DIF)
+# dtAmostra$Credito_Salario_DIF <- as.factor(dtAmostra$Credito_Salario_DIF)
+# dtAmostra$Caixa_Seguradora_DIF <- as.factor(dtAmostra$Caixa_Seguradora_DIF)
+# 
+# str(dtAmostra)
 
-dtAmostra$Evadiu <- as.factor(dtAmostra$Evadiu)
-dtAmostra$Segmento <- as.factor(dtAmostra$Segmento)
-dtAmostra$Debito_Automatico <- as.factor(dtAmostra$Debito_Automatico)
-dtAmostra$Credito_Salario <- as.factor(dtAmostra$Credito_Salario)
-dtAmostra$Credenciamento <- as.factor(dtAmostra$Credenciamento)
-dtAmostra$Caixa_Seguradora <- as.factor(dtAmostra$Caixa_Seguradora)
-dtAmostra$Pediu_Portabilidade <- as.factor(dtAmostra$Pediu_Portabilidade)
-dtAmostra$Abriu_Reclamacao <- as.factor(dtAmostra$Abriu_Reclamacao)
-dtAmostra$Debito_Automatico_DIF <- as.factor(dtAmostra$Debito_Automatico_DIF)
-dtAmostra$Credito_Salario_DIF <- as.factor(dtAmostra$Credito_Salario_DIF)
-dtAmostra$Caixa_Seguradora_DIF <- as.factor(dtAmostra$Caixa_Seguradora_DIF)
-
-str(dtAmostra)
-
+###########################################################################
+###########################################################################
 ### Aplicação da função Winsor para diminuir os outliers existentes na base de dados
 
-dtAmostra = dtAmostra %>% 
-  mutate(winsored_Rentabilidade_PERC = psych::winsor(Rentabilidade_PERC, 0.1)) %>%
-  mutate(winsored_Produtos = psych::winsor(Produtos, 0.01)) %>%
-  mutate(winsored_Produtos_Qualificados_PERC = psych::winsor(Produtos_Qualificados_PERC, 0.01)) %>%
-  mutate(winsored_Produtos_PERC = psych::winsor(Produtos_PERC, 0.01)) %>%
-  mutate(winsored_Movimentacoes = psych::winsor(Movimentacoes, 0.1)) %>%
-  mutate(winsored_Movimentacoes_Anterior = psych::winsor(Movimentacoes_Anterior, 0.1)) %>%
-  mutate(winsored_Movimentacoes_DIF = psych::winsor(Movimentacoes_DIF, 0.1)) %>%
-  mutate(winsored_Movimentacoes_PERC = psych::winsor(Movimentacoes_PERC, 0.1)) %>%
-  mutate(winsored_Aplicacao = psych::winsor(Aplicacao, 0.1)) %>%
-  mutate(winsored_Aplicacao_Anterior = psych::winsor(Aplicacao_Anterior, 0.1)) %>%
-  mutate(winsored_Aplicacao_DIF = psych::winsor(Aplicacao_DIF, 0.1)) %>% 
-  mutate(winsored_Aplicacao_PERC = psych::winsor(Aplicacao_PERC, 0.1)) %>% 
-  mutate(winsored_Credito = psych::winsor(Credito, 0.1)) %>% 
-  mutate(winsored_Credito_Anterior = psych::winsor(Credito_Anterior, 0.1)) %>% 
-  mutate(winsored_Credito_DIF = psych::winsor(Credito_DIF, 0.1)) %>% 
-  mutate(winsored_Credito_PERC = psych::winsor(Credito_PERC, 0.1)) %>% 
-  mutate(winsored_Rentabilidade = psych::winsor(Rentabilidade, 0.1)) %>% 
-  mutate(winsored_Rentabilidade_Anterior = psych::winsor(Rentabilidade_Anterior, 0.1)) %>% 
-  mutate(winsored_Rentabilidade_DIF = psych::winsor(Rentabilidade_DIF, 0.1)) %>% 
-  mutate(winsored_Rentabilidade_PERC = psych::winsor(Rentabilidade_PERC, 0.1))
+# dtAmostra = dtAmostra %>% 
+#   mutate(winsored_Rentabilidade_PERC = psych::winsor(Rentabilidade_PERC, 0.1)) %>%
+#   mutate(winsored_Produtos = psych::winsor(Produtos, 0.01)) %>%
+#   mutate(winsored_Produtos_Qualificados_PERC = psych::winsor(Produtos_Qualificados_PERC, 0.01)) %>%
+#   mutate(winsored_Produtos_PERC = psych::winsor(Produtos_PERC, 0.01)) %>%
+#   mutate(winsored_Movimentacoes = psych::winsor(Movimentacoes, 0.1)) %>%
+#   mutate(winsored_Movimentacoes_Anterior = psych::winsor(Movimentacoes_Anterior, 0.1)) %>%
+#   mutate(winsored_Movimentacoes_DIF = psych::winsor(Movimentacoes_DIF, 0.1)) %>%
+#   mutate(winsored_Movimentacoes_PERC = psych::winsor(Movimentacoes_PERC, 0.1)) %>%
+#   mutate(winsored_Aplicacao = psych::winsor(Aplicacao, 0.1)) %>%
+#   mutate(winsored_Aplicacao_Anterior = psych::winsor(Aplicacao_Anterior, 0.1)) %>%
+#   mutate(winsored_Aplicacao_DIF = psych::winsor(Aplicacao_DIF, 0.1)) %>% 
+#   mutate(winsored_Aplicacao_PERC = psych::winsor(Aplicacao_PERC, 0.1)) %>% 
+#   mutate(winsored_Credito = psych::winsor(Credito, 0.1)) %>% 
+#   mutate(winsored_Credito_Anterior = psych::winsor(Credito_Anterior, 0.1)) %>% 
+#   mutate(winsored_Credito_DIF = psych::winsor(Credito_DIF, 0.1)) %>% 
+#   mutate(winsored_Credito_PERC = psych::winsor(Credito_PERC, 0.1)) %>% 
+#   mutate(winsored_Rentabilidade = psych::winsor(Rentabilidade, 0.1)) %>% 
+#   mutate(winsored_Rentabilidade_Anterior = psych::winsor(Rentabilidade_Anterior, 0.1)) %>% 
+#   mutate(winsored_Rentabilidade_DIF = psych::winsor(Rentabilidade_DIF, 0.1)) %>% 
+#   mutate(winsored_Rentabilidade_PERC = psych::winsor(Rentabilidade_PERC, 0.1))
 
+###########################################################################
+###########################################################################
 ### Execução dos graficos com as variáveis "melhoradas" (winsorizadas)
 
-variaveisIniciais <- c("Produtos",
-                       "Produtos_Anterior",
-                       "Produtos_DIF",
-                       "winsored_Produtos_Qualificados_PERC",
-                       "Produtos_Qualificados_DIF",
-                       "Produtos_Qualificados_Anterior",
-                       "Produtos_Qualificados",
-                       "winsored_Produtos_PERC",
-                       "winsored_Aplicacao",
-                       "winsored_Aplicacao_Anterior",
-                       "winsored_Aplicacao_PERC", 
-                       "winsored_Aplicacao_DIF",
-                       "winsored_Movimentacoes",
-                       "winsored_Movimentacoes_DIF",
-                       "winsored_Movimentacoes_PERC", 
-                       "winsored_Movimentacoes_Anterior", 
-                       "winsored_Credito", 
-                       "winsored_Credito_Anterior", 
-                       "winsored_Credito_PERC", 
-                       "winsored_Credito_DIF", 
-                       "winsored_Rentabilidade", 
-                       "winsored_Rentabilidade_DIF", 
-                       "winsored_Rentabilidade_Anterior", 
-                       "winsored_Rentabilidade_PERC")
+# variaveisIniciais <- c("Produtos",
+#                        "Produtos_Anterior",
+#                        "Produtos_DIF",
+#                        "winsored_Produtos_Qualificados_PERC",
+#                        "Produtos_Qualificados_DIF",
+#                        "Produtos_Qualificados_Anterior",
+#                        "Produtos_Qualificados",
+#                        "winsored_Produtos_PERC",
+#                        "winsored_Aplicacao",
+#                        "winsored_Aplicacao_Anterior",
+#                        "winsored_Aplicacao_PERC", 
+#                        "winsored_Aplicacao_DIF",
+#                        "winsored_Movimentacoes",
+#                        "winsored_Movimentacoes_DIF",
+#                        "winsored_Movimentacoes_PERC", 
+#                        "winsored_Movimentacoes_Anterior", 
+#                        "winsored_Credito", 
+#                        "winsored_Credito_Anterior", 
+#                        "winsored_Credito_PERC", 
+#                        "winsored_Credito_DIF", 
+#                        "winsored_Rentabilidade", 
+#                        "winsored_Rentabilidade_DIF", 
+#                        "winsored_Rentabilidade_Anterior", 
+#                        "winsored_Rentabilidade_PERC")
+# 
+# box_plot(credit, variaveisIniciais, NULL)
 
-box_plot(credit, variaveisIniciais, NULL)
-
+###########################################################################
+###########################################################################
 ### Crialçao de uma lista contendo aquelas variaveis que são fatores (não tiveram boa relação com a variável preditora)
 
-variaveisSubgrupo <- c("Segmento", "Credenciamento", "Caixa_Seguradora", "Caixa_Seguradora_DIF", "Credito_Salario", "Debito_Automatico", "Credito_Salario_DIF", "Debito_Automatico_DIF")
+# variaveisSubgrupo <- c("Segmento", "Credenciamento", 
+#                        "Caixa_Seguradora", "Caixa_Seguradora_DIF", 
+#                        "Credito_Salario", "Debito_Automatico", 
+#                        "Credito_Salario_DIF", "Debito_Automatico_DIF")
+# 
+# box_plot(credit, c("winsored_Movimentacoes", "Produtos_Qualificados", 
+#                    "winsored_Aplicacao_PERC", "winsored_Credito_DIF"), variaveisSubgrupo)
 
-box_plot(credit, c("winsored_Movimentacoes", "Produtos_Qualificados", "winsored_Aplicacao_PERC", "winsored_Credito_DIF"), variaveisSubgrupo)
-
+###########################################################################
+###########################################################################
 ### Início dos trabalhos para a regressão... criação de variáveis dummies
 
 str(dtAmostra)
@@ -165,11 +184,7 @@ dtAmostra =
   dtAmostra %>%
   mutate_if(is.ordered, as.numeric) %>%
   mutate(EvadiuTipo = as.numeric(Evadiu)) %>%
-  # mutate(Segmento_Num1 = ifelse(Segmento == "GV", 1, ifelse(Segmento == "GC", 2, ifelse(Segmento == "GR", 3, 4)))) %>% 
-  # mutate(Segmento_Num = as.factor(Segmento_Num1)) %>% 
-  # select(-Id, -Evadiu, -Evadiu_formatado, -Segmento_Descricao, -Segmento, -Segmento_Num1, -Pediu_Portabilidade, -Abriu_Reclamacao) %>%
   select(-Id, -Pediu_Portabilidade, -Abriu_Reclamacao, -Evadiu) %>%
-  # select(-Id, -Evadiu, -Segmento, -Segmento_Num1, -Pediu_Portabilidade, -Abriu_Reclamacao) %>%
   drop_na() %>%
   as.data.table()
 
@@ -212,7 +227,9 @@ control
 # str(preprocessed_data)
 # stargazer::stargazer(data, type = "text")
 
-### Rodando uma árvore de decisão com repeated k-fold cross-validation
+###########################################################################
+###########################################################################
+### Rodando uma árvore de decisão com repeated k-fold cross-validation-- 1ª FORMA
 ### Função de maximização: ROC
 model_arvoreDecisao1 = caret::train(EvadiuTipo ~ .,
                                     data         = data,
@@ -220,25 +237,15 @@ model_arvoreDecisao1 = caret::train(EvadiuTipo ~ .,
                                     metric       = "ROC",
                                     method       = 'rpart',
                                     tuneLength   = 5)
-model_arvoreDecisao1
 
-### A cara da árvore de decisão...
-rattle::fancyRpartPlot(model_arvoreDecisao1$finalModel)
-
-### Vamos ver os parâmetros da árvore de decisão (que podem ser otimizados)
-# modelLookup('rpart')
-
-### Rodando uma árvore de decisão com repeated k-fold cross-validation -- 2ª TENTATIVA
+### Rodando uma árvore de decisão com repeated k-fold cross-validation -- 2ª FORMA
 model_arvoreDecisao2 = caret::train(EvadiuTipo ~ .,
                                     data         = data,
                                     trControl    = control,
                                     metric       = "ROC",
                                     method       = 'rpart')
 
-### A cara da árvore de decisão...
-rattle::fancyRpartPlot(model_arvoreDecisao2$finalModel)
-
-### Rodando uma árvore de decisão com repeated k-fold cross-validation -- 3ª TENTATIVA
+### Rodando uma árvore de decisão com repeated k-fold cross-validation -- 3ª FORMA
 grid.search = expand.grid(cp = seq(0, 0.01, length.out = 20))
 model_arvoreDecisao3 = caret::train(EvadiuTipo ~ .,
                                     data         = data,
@@ -247,10 +254,17 @@ model_arvoreDecisao3 = caret::train(EvadiuTipo ~ .,
                                     method       = 'rpart',
                                     tuneGrid     = grid.search)
 
+### Vamos ver os parâmetros da árvore de decisão (que podem ser otimizados)
+# modelLookup('rpart')
+
 ### A cara da árvore de decisão...
+rattle::fancyRpartPlot(model_arvoreDecisao1$finalModel)
+rattle::fancyRpartPlot(model_arvoreDecisao2$finalModel)
 rattle::fancyRpartPlot(model_arvoreDecisao3$finalModel)
 
-### Juntando todos os classificadores em um comando só ----------------------
+###########################################################################
+###########################################################################
+### Juntando todos os classificadores em um comando só
 models =
   caretList(EvadiuTipo ~ .,
             data = data,
